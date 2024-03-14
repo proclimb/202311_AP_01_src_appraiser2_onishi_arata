532f55b4eb6103dded56f98188178c320c677136
onishi <202311ap01_onishi@x-ide.sakura.ne.jp>
2024-03-14 15:05:41 +0900

    不要な全角スペースを除去

--- SRC/libArticle.php
+++ SRC/libArticle.php
@@ -260,7 +260,7 @@ function subArticleEdit()
 			</tr>
 		</table>
 
-		<a href="javascript:fnArticleEditCheck();"><img src="./images/<?php print $btnImage ?>" /></a>　
+		<a href="javascript:fnArticleEditCheck();"><img src="./images/<?php print $btnImage ?>" /></a>
 		<a href="javascript:form.act.value='articleSearch';form.submit();"><img src="./images/btn_return.png" /></a>
 		<?php
 		if ($articleNo) {
